1fdba1066e211b022a5a103bef6f51da1f738c1b
kylead10 <adiokayode111@gmail.com>
2024-01-10 21:33:19 +0100

    h1

--- image_search/src/App.jsx
+++ image_search/src/App.jsx
@@ -3,7 +3,12 @@ import { Form } from 'react-bootstrap';
 import './index.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='container'>
+      <h1 className='title'>Image Searcher</h1>
+      <div className='search-section'></div>
+    </div>
+  );
 };
 
 export default App;
